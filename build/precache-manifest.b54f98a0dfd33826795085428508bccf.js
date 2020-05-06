fccebc97ee34947ae44f501bdcd348eb738b5903
aayushishere <aayushsingh2203@gmail.com>
2020-05-06 23:44:06 +0530

    changing folders

---
+++ build/precache-manifest.b54f98a0dfd33826795085428508bccf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7a78dc7ded96fac5ce5bf36e700472",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ce3e41422618ef54486",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "53169f9ca2a6c563d2c8",
+    "url": "/static/css/main.bad0c0bd.chunk.css"
+  },
+  {
+    "revision": "3ce3e41422618ef54486",
+    "url": "/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53169f9ca2a6c563d2c8",
+    "url": "/static/js/main.a818ac31.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  }
+]);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary'
 import './App.css';
 
 class App extends Component {
@@ -24,12 +25,13 @@ class App extends Component {
 		this.setState({ searchfield: event.target.value})
 	}
 	render(){
-		const filteredRobots = this.state.robots.filter(robots =>{
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { robots,searchfield } = this.state;
+		const filteredRobots = robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 			
 		})
-		if(this.state.robots.length === 0) {
-			return <h1 className='tc'>LOADING...</h1>
+		if(!robots.length) {
+			return <h1>LOADING...</h1>
 		} 
 		else{
 			return(
@@ -37,7 +39,9 @@ class App extends Component {
 				<h1 className = 'f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<Scroll>
-				<CardList robots={filteredRobots}/>
+					<ErrorBoundary>
+					<CardList robots={filteredRobots}/>
+					</ErrorBoundary>
 				</Scroll> 
 			</div>
 			);
